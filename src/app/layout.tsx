adb7f0dab383cc2ea7abe949d4b6a232b3d6da2e
Emmanuel Utomi <emmanuelutomi@gmail.com>
2023-11-16 11:06:11 -0600

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,22 @@ export const metadata = {
   },
 }
 
+const Gtag = () => {
+  if (typeof process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS !== "undefined")
+    return (<>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script id="google-analytics">
+        {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+`}
+      </Script></>)
+}
+
 
 export default function RootLayout({
   children,
@@ -23,17 +39,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
 
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-        <Script id="google-analytics">
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-        </Script>
+        <Gtag />
 
         {children}</body>
     </html>
