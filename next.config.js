a7b8eed7149f5ea60a562cd75ef506b571e1e0dc
Emmanuel Utomi <emmanuelutomi@gmail.com>
2023-12-07 16:27:40 -0600

    push

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  distDir: '_out',
+  output: 'export',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
